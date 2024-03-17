579f1cec1d0da24c6146cb0934ba179f3263485e
DestroyerV <mishravaibhav12321@gmail.com>
2024-03-18 01:36:20 +0530

    clock project for learning npm

---
+++ script.js
@@ -0,0 +1,27 @@
+dayjs.extend(window.dayjs_plugin_timezone);
+dayjs.extend(window.dayjs_plugin_utc);
+
+let timezone = dayjs.tz.guess();
+
+
+
+function changeTimezone() {
+  timezone = document.querySelector("select").value;
+  document.querySelector(".timezone h3").innerHTML = timezone;
+}
+
+apply.addEventListener("click", () => {
+  changeTimezone();
+});
+
+setInterval(() => {
+  document.querySelector(".timezone h3").innerHTML = timezone;
+  const time = dayjs().tz(timezone).format("HH:mm:ss");
+  document.querySelector(".time h1").innerHTML = time;
+  const date = dayjs().tz(timezone).format("dddd, D MMMM, YYYY");
+  document.querySelector(".date").innerHTML = date;
+}, 1000);
+
+
+
+MicroModal.init();
